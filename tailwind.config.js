0c23a8d925fc13d77f0f75f4aa4b069ae3accf41
Victor Batista <victorbtst77@gmail.com>
2024-11-02 23:30:25 -0300

    feat: Setup de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-dark': '#17192D',
+        'neutral-gray': {
+          950: '#24292F'
+        },
+        blue: {
+          100: '#F0F5FF',
+          200: '#C2D6FF',
+          300: '#94B7FF',
+          400: '#6598FF',
+          500: '#2188FF',
+          600: '#0A5AFF',
+          700: '#0848CC',
+          800: '#063A9A',
+          900: '#023B78',
+        } 
+      },
+    },
   },
   plugins: [],
 }
