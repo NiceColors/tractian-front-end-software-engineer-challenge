d91e7f50a9b71a401bf2f231341ec4137c34fd5f
Victor Batista <victorbtst77@gmail.com>
2024-11-07 13:07:27 -0300

    fix: Import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  }
 });
